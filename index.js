54f67b0ffb5719b230b0a60f1e5d5c9b44477afd
Toni T. Diep <60229738+tonitdiep@users.noreply.github.com>
2022-02-03 23:09:36 +0000

    question

---
+++ index.js
@@ -0,0 +1,20 @@
+
+/**
+ * @param {number[]} arr
+ * @return {boolean}
+ */
+var threeConsecutiveOdds = function(arr) {
+
+}
+
+
+
+/*
+QUESTION #1550: Given an integer array arr, return true if there are three consecutive odd numbers in the array. Otherwise, return false.
+
+EXAMPLE: Input: arr = [1,2,34,3,4,5,7,23,12]
+Output: true
+Explanation: [5,7,23] are three consecutive odds.
+
+
+ */
