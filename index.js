1e31915012ef251ea8947ec2aefabe2432db6be0
Toni T. Diep <60229738+tonitdiep@users.noreply.github.com>
2022-02-03 23:13:11 +0000

    solution

--- index.js
+++ index.js
@@ -3,8 +3,24 @@
  * @param {number[]} arr
  * @return {boolean}
  */
-var threeConsecutiveOdds = function(arr) {
+var threeConsecutiveOdds = function(arr = [1,2,34,3,4,5,7,23,12]) {
+   let count = 0;
+    
+    for(let i = 0; i < arr.length; i++){
+       if (arr[i] % 2 == 1){
+          
+           count++;
+     
+       } else {
+           count = 0;
+       }
+        if (count == 3) {
+             
+            return true
+        }
+     }
 
+    return false
 }
 
 
